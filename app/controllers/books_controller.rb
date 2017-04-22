30138c1fa5e38cf73743a7baa911edbbade4e351
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-22 15:48:53 +0700

    add category

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,10 +11,12 @@ class BooksController < ApplicationController
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.order("name ASC").map{ |c| [c.name, c.id] }
   end
 
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
 
     if @book.save
       redirect_to root_path
@@ -24,9 +26,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -42,7 +46,7 @@ class BooksController < ApplicationController
 private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
 
   def find_book

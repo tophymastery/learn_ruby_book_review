da0d2587079a8560db3e5942cbf437036f358fa8
Akadet Mathurapoj <akadet85@gmail.com>
2017-04-23 13:21:42 +0700

    fixed - not signed user go to book edit will redirect to sign in page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
-  before_action :find_book, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:category].blank?
